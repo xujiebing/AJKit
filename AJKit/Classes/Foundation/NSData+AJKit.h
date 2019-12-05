6994588162a121cfb4452d8df68f2cadefa75611
xujiebing <xujiebing1992@gmail.com>
2019-12-05 11:02:05 +0800

    修改NSData

--- AJKit/Classes/Foundation/NSData+AJKit.h
+++ AJKit/Classes/Foundation/NSData+AJKit.h
@@ -12,29 +12,24 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSData (AJKit)
 
 /// 判断NSData是否为空
-/// @param NSData* data对象
 /// @return BOOL 是否为空
-+ (BOOL (^)(NSData *ajSelf))ajIsEmpty;
+- (BOOL)ajIsEmpty;
 
 /// 将NSData对象转换为十六进制的字符串
-/// @param NSData* data对象
 /// @return NSString* 十六进制的字符串或nil
-+ (NSString *(^)(NSData *ajSelf))ajHexString;
+- (NSString *)ajHexString;
 
 /// 将NSData转换成一个base64格式的字符串
-/// @param NSData* data对象
 /// @return NSString* base64格式的字符串或nil
-+ (NSString *(^)(NSData *ajSelf))ajBase64Encode;
+- (NSString *)ajBase64Encode;
 
 /// 将NSData转换成json对象
-/// @param NSData* data对象
 /// @return id json对象或nil
-+ (id (^)(NSData *ajSelf))ajJsonValueDecoded;
+- (id)ajJsonValueDecoded;
 
 /// 将NSData转换成字符串（UTF8格式）
-/// @param NSData* data对象
 /// @return NSString* 字符串或nil
-+ (NSString *(^)(NSData *ajSelf))ajStringValue;
+- (NSString *)ajStringValue;
 
 @end
 
--- AJKit/Classes/Foundation/NSData+AJKit.m
+++ AJKit/Classes/Foundation/NSData+AJKit.m
@@ -11,104 +11,85 @@ static const char base64EncodingTable[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghi
 
 @implementation NSData (AJKit)
 
-+ (BOOL (^)(NSData * _Nonnull))ajIsEmpty {
-    BOOL (^block)(NSData *) = ^(NSData *ajSelf) {
-        if (![ajSelf isKindOfClass:NSData.class]) {
-            return YES;
-        }
-        if (ajSelf.length == 0) {
-            return YES;
-        }
-        return NO;
-    };
-    return block;
+- (BOOL)ajIsEmpty {
+    if (self.length == 0) {
+        return YES;
+    }
+    return NO;
 }
 
-+ (NSString * _Nonnull (^)(NSData * _Nonnull))ajHexString {
-    NSString * (^block)(NSData *) = ^(NSData *ajSelf){
-        if (NSData.ajIsEmpty(ajSelf)) {
-            return AJEmptyString;
+- (NSString *)ajHexString {
+    if (self.ajIsEmpty) {
+        return nil;
+    }
+    Byte *bytes = (Byte *)[self bytes];
+    NSString *hexStr = @"";
+    for(int i=0; i<[self length]; i++) {
+        NSString *newHexStr = [NSString stringWithFormat:@"%x",bytes[i] & 0xff]; //16进制数
+        newHexStr = [newHexStr uppercaseString];
+        if([newHexStr length] == 1) {
+            newHexStr = [NSString stringWithFormat:@"0%@",newHexStr];
         }
-        Byte *bytes = (Byte *)[ajSelf bytes];
-        NSString *hexStr = @"";
-        for(int i=0; i<[ajSelf length]; i++) {
-            NSString *newHexStr = [NSString stringWithFormat:@"%x",bytes[i] & 0xff]; //16进制数
-            newHexStr = [newHexStr uppercaseString];
-            if([newHexStr length] == 1) {
-                newHexStr = [NSString stringWithFormat:@"0%@",newHexStr];
+        hexStr = [hexStr stringByAppendingString:newHexStr];
+    }
+    return hexStr;
+}
+
+- (NSString *)ajBase64Encode {
+    if (self.ajIsEmpty) {
+        return nil;
+    }
+    NSUInteger length = self.length;
+    NSUInteger out_length = ((length + 2) / 3) * 4;
+    uint8_t *output = malloc(((out_length + 2) / 3) * 4);
+    if (output == NULL) {
+        return nil;
+    }
+    const char *input = self.bytes;
+    NSInteger i, value;
+    for (i = 0; i < length; i += 3) {
+        value = 0;
+        for (NSInteger j = i; j < i + 3; j++) {
+            value <<= 8;
+            if (j < length) {
+                value |= (0xFF & input[j]);
             }
-            hexStr = [hexStr stringByAppendingString:newHexStr];
         }
-        return hexStr;
-    };
-    return block;
+        NSInteger index = (i / 3) * 4;
+        output[index + 0] = base64EncodingTable[(value >> 18) & 0x3F];
+        output[index + 1] = base64EncodingTable[(value >> 12) & 0x3F];
+        output[index + 2] = ((i + 1) < length)
+        ? base64EncodingTable[(value >> 6) & 0x3F]
+        : '=';
+        output[index + 3] = ((i + 2) < length)
+        ? base64EncodingTable[(value >> 0) & 0x3F]
+        : '=';
+    }
+    NSString *base64 = [[NSString alloc] initWithBytes:output
+                                                length:out_length
+                                              encoding:NSASCIIStringEncoding];
+    free(output);
+    return base64;
 }
 
-+ (NSString * _Nonnull (^)(NSData * _Nonnull))ajBase64Encode {
-    NSString * (^block)(NSData *) = ^(NSData *ajSelf){
-        if (NSData.ajIsEmpty(ajSelf)) {
-            return AJEmptyString;
-        }
-        NSUInteger length = ajSelf.length;
-        NSUInteger out_length = ((length + 2) / 3) * 4;
-        uint8_t *output = malloc(((out_length + 2) / 3) * 4);
-        if (output == NULL) {
-            return AJEmptyString;
-        }
-        const char *input = ajSelf.bytes;
-        NSInteger i, value;
-        for (i = 0; i < length; i += 3) {
-            value = 0;
-            for (NSInteger j = i; j < i + 3; j++) {
-                value <<= 8;
-                if (j < length) {
-                    value |= (0xFF & input[j]);
-                }
-            }
-            NSInteger index = (i / 3) * 4;
-            output[index + 0] = base64EncodingTable[(value >> 18) & 0x3F];
-            output[index + 1] = base64EncodingTable[(value >> 12) & 0x3F];
-            output[index + 2] = ((i + 1) < length)
-            ? base64EncodingTable[(value >> 6) & 0x3F]
-            : '=';
-            output[index + 3] = ((i + 2) < length)
-            ? base64EncodingTable[(value >> 0) & 0x3F]
-            : '=';
-        }
-        NSString *base64 = [[NSString alloc] initWithBytes:output
-                                                    length:out_length
-                                                  encoding:NSASCIIStringEncoding];
-        free(output);
-        return base64;
-    };
-    return block;
+- (id)ajJsonValueDecoded {
+    if (self.ajIsEmpty) {
+        return nil;
+    }
+    NSError *error = nil;
+    id value = [NSJSONSerialization JSONObjectWithData:self options:kNilOptions error:&error];
+    if (error) {
+        AJLog(@"jsonValueDecoded error:%@", error);
+    }
+    return value;
 }
 
-+ (id _Nonnull (^)(NSData * _Nonnull))ajJsonValueDecoded {
-    id (^block)(NSData *) = ^(NSData *ajSelf){
-        id value = nil;
-        if (NSData.ajIsEmpty(ajSelf)) {
-            return value;
-        }
-        NSError *error = nil;
-        value = [NSJSONSerialization JSONObjectWithData:ajSelf options:kNilOptions error:&error];
-        if (error) {
-            AJLog(@"jsonValueDecoded error:%@", error);
-        }
-        return value;
-    };
-    return block;
-}
-
-+ (NSString * _Nonnull (^)(NSData * _Nonnull))ajStringValue {
-    NSString * (^block)(NSData *) = ^(NSData *ajSelf){
-        if (NSData.ajIsEmpty(ajSelf)) {
-            return AJEmptyString;
-        }
-        NSString *utf8String = [[NSString alloc] initWithData:ajSelf encoding:NSUTF8StringEncoding];
-        return utf8String;
-    };
-    return block;
+- (NSString *)ajStringValue {
+    if (self.ajIsEmpty) {
+        return nil;
+    }
+    NSString *utf8String = [[NSString alloc] initWithData:self encoding:NSUTF8StringEncoding];
+    return utf8String;
 }
 
 @end
